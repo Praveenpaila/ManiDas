ed81554ec2ee0dfe8a38ca7c41643f87799233af
Nishant <nishantlugalapu@gmail.com>
2026-03-08 07:00:13 +0530

    images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,7 @@
-import hero from "./image.png";
+import badam from "./badam.png";
+import bm from "./bm.png";
+import cc from "./cc.png";
+import eo from "./eo.png";
 import hero1 from "./hero1.png";
 import hero2 from "./hero2.png";
 import hero3 from "./hero3.png";
@@ -6,23 +9,43 @@ import hero4 from "./hero4.png";
 import hero5 from "./hero5.png";
 import hero6 from "./hero6.png";
 import hero7 from "./hero7.png";
-import one from "./one.mp4";
-import two from "./two.mp4";
-import three from "./three.mp4";
+import hero from "./image.png";
 import logo from "./logo.png";
-import badam from "./badam.png";
 import milk from "./milk.png";
 import milk2 from "./milk2.png";
-import bm from "./bm.png";
-import cc from "./cc.png";
-import eo from "./eo.png";
+import o1 from "./o1.png";
+import o10 from "./o10.png";
+import o11 from "./o11.png";
+import o12 from "./o12.png";
+import o13 from "./o13.png";
+import o14 from "./o14.png";
+import o15 from "./o15.png";
+import o16 from "./o16.png";
+import o17 from "./o17.png";
+import o18 from "./o18.png";
+import o19 from "./o19.png";
+import o2 from "./o2.png";
+import o20 from "./o20.png";
+import o21 from "./o21.png";
+import o22 from "./o22.png";
+import o23 from "./o23.png";
+import o24 from "./o24.png";
+import o25 from "./o25.png";
+import o3 from "./o3.png";
+import o4 from "./o4.png";
+import o5 from "./o5.png";
+import o6 from "./o6.png";
+import o7 from "./o7.png";
+import o8 from "./o8.png";
+import o9 from "./o9.png";
+import one from "./one.mp4";
+import three from "./three.mp4";
+import two from "./two.mp4";
 const images = {
   badam,
   milk,
   milk2,
-  bm,
-  cc,
-  eo,
+  bm,cc,eo,o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11,o12,o13,o14,o15,o16,o17,o18,o19,o20,o21,o22,o23,o24,o25,
 };
 const products = {
   hero,
